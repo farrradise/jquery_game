7cbd9629bebb79a100c343ad868645729d99e4f6
Farrah <farrradise>
2018-03-01 17:15:29 +0100

    continuing the bubble animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
 $(function(){
 
+  var screenWidth = $(document).width(), bubbles = ['bulle_1', 'bulle_2', 'bulle_3', 'bulle_4'];
+  var countBubble = 0;
+
   //bubbles when doc is ready
-  $('body').prepend("<img src='img/bulle_1.png' alt='bubbles'>")
+  setInterval(function(){
+    var imgBubble = bubbles[Math.floor(Math.random()*bubbles.length)];
+    var randomLeft = Math.floor(Math.random()*screenWidth);
+    $('body').prepend("<img src='img/"+imgBubble+".png' alt='bubbles'>");
+
+    var lastImg = $('body > img:first-of-type');
+    lastImg.offset( {left: randomLeft });
+    lastImg.animate( 7000, function () {
+      $(this).css({'top', -=100vh;'});
+    });
+    // lastImg.offset().left(400);
+    // $('body > img:first-of-type').animate(3500, function() {
+      // $(this).css('top', '100vh');
+      // $(this).css('border', '3px solid yellow');
+    // });
+
+    // xImg = $('body img').offset().
+    // $('body img').animate({params},speed,callback);;
+    // alert(xImg.left);
+    // alert(countBubble);
+    // countBubble++;
+
+  }, 200);
   // ajouter une image toutes les 1500ms
   // image au hasard entre 1 et 4
-   
+  // css mettre un position absolute top - 40px
+  // prendre un left au hasard par rapport à la window
+
 
   // start the game on click play
   $('.start').click(function (){
@@ -41,7 +68,7 @@ function startFadeOut() {
          }, 750);
 
          // mettre ici appel au demarrage du jeu
-         alert('test');
+         // alert('test');
        });
      }
      index++;
