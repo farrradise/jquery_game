4a2bd6c30f1b68cb3f587eee710950a1e08cb361
Farrah <farrradise>
2018-03-06 15:38:08 +0100

    continuing the fish animation

--- js/main.js
+++ js/main.js
@@ -2,16 +2,17 @@
 var index = 0;
 var screenWidth = $(document).width();
 var screenHeight = $(document).height();
+var startGame = [3, 2, 1, "START"];
 var bubbles = ['bulle_1', 'bulle_2', 'bulle_3', 'bulle_4'];
 var countBubble = 0;
-
-
+var fish;
+var speed = 8000;
 
 $(function(){
 
 
   //bubbles when doc is ready
-  setInterval(function(){ bubble(); }, 200);
+  // setInterval(function(){ bubble(); }, 200);
 
 
   // start the game on click play
@@ -20,7 +21,7 @@ $(function(){
       function(){
         $(this).remove();
         $(".start").html('<span></span>');
-        $(".start span").addClass('test');
+        // $(".start span").addClass('');
 
         startFadeOut();
     });
@@ -53,21 +54,25 @@ $(function(){
 
 function startFadeOut() {
 
-  var startGame = [3, 2, 1, "START"];
   setTimeout(function () {
     $(".start span").fadeIn(300).delay(900).fadeOut(300);
     $(".start span").text(startGame[index]);
 
      if (index != startGame.length - 1) {
         startFadeOut();
-     } else {
+     }
+     else {
        $(".start").fadeOut(1450, function () {
          setTimeout(function() {
            $(".start").remove();
          }, 750);
 
          // mettre ici appel au demarrage du jeu
-         // alert('test');
+         setInterval(function(){ gameIsStarted(); }, 4000 );
+         setInterval(function(){ if (speed < 7900) {
+           setInterval(function(){ gameIsStarted(); }, 4000 );
+         } }, 2000 );
+
        });
      }
      index++;
@@ -91,4 +96,36 @@ function bubble() {
   } else {
     lastImg.animate({top : '-=120vh'}, 12000, function(){$(this).remove();});
   }
+}
+
+
+function gameIsStarted() {
+  var randomLeft = Math.floor(Math.random()*450);
+
+  // Fishes
+  if (fish == "poisson1") {
+    fish = 'poisson2';
+  }  else {
+    fish = 'poisson1';
+  }
+
+  $('main .surfaceGame').prepend("<img src='img/"+fish+".png' class=\"poisson\" alt='fish'>");
+  var lastFish = $('main .surfaceGame img:first-of-type');
+  var offsetLastFish = lastFish.offset();
+  var offsetLeftLastFish = Math.floor(offsetLastFish.left);
+  lastFish.offset({left : randomLeft + offsetLeftLastFish })
+  lastFish.animate({top : '+=450'}, speed, function(){$(this).remove();});
+  if (speed > 1000) {
+    speed -= 50;
+  }
+  // speed2 -=350;
+  // alert(speed2);
+  // meme principe que pour les bulles : faire un set interval MAIS un interval de plus en plus petit
+  // animer ce poisson pour qu'il descende jusqu'en bas de la surface de jeu div.surfaceGame
+  // le supprimer quand il n'est plus dans la surface de jeu
+
+
+
+  // trashes
+  //meme principe que pour les poissons mais un autre set interval
 }
